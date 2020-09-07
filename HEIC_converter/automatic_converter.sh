bd3b2faec099307df394de9a8af3ae84bf05d5e1
Ali Raeisdanaei <mrrookie2@gmail.com>
2020-09-06 21:58:53 -0400

    Making file for LivePhotoConverter

--- HEIC_converter/automatic_converter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-for f in $1/*; do
-~/Pictures/Tools/HEIC_converter/tifig -v -p "$f" "${f%.*}.jpg"
-done
-
-rm $1/*.HEIC
---
+++ LivePhotoConverter/auto_MOV_converter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+for f in $1/*; do
+
+done
+
+rm $1/*.HEIC
